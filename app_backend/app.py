257ef74fcd32a3e6779e36a9602e2b54fe15fe53
mtegovski <Miha_107>
2022-07-13 20:34:21 +0200

    endpoints created

--- app_backend/app.py
+++ app_backend/app.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import Flask, session, request
+
+app = Flask(__name__)
+
+
+@app.route('/api/home')
+def get_disease():
+    return session['disease']
+
+
+@app.route('/api/disease', methods=['POST'])
+def submit_disease():
+    if request.method == 'POST':
+        session['disease'] = request.json['disease']
+        return session['disease']
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'memcached'
+    app.run(debug=True)
