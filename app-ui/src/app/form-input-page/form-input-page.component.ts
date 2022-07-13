27c010bc4e2e396e05fc524483e9c16c6cb59a4e
mtegovski <Miha_107>
2022-07-13 19:54:17 +0200

    ng form component created

---
+++ app-ui/src/app/form-input-page/form-input-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-input-page',
+  templateUrl: './form-input-page.component.html',
+  styleUrls: ['./form-input-page.component.scss']
+})
+export class FormInputPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
